77cb375ded78373a0ea3a17b85d5f7cced038ab1
veryfastman <dony357@outlook.com>
2023-12-19 22:00:27 -0800

    added and configured harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,32 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  keys = {
+    { "<leader>ha", function() require("harpoon"):list():append() end, desc = "Add file to Harpoon" },
+    { "<leader>hh", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Toggle Harpoon's quick menu" },
+    { "<leader>h1", function() require("harpoon"):list():select(1) end, desc = "Navigate to mark 1" },
+    { "<leader>h2", function() require("harpoon"):list():select(2) end, desc = "Navigate to mark 2" },
+    { "<leader>h3", function() require("harpoon"):list():select(3) end, desc = "Navigate to mark 3" },
+    { "<leader>h4", function() require("harpoon"):list():select(4) end, desc = "Navigate to mark 4" }
+  },
+  init = function ()
+    require("harpoon"):setup()
+  end
+}
+
+--[[ return {
+  "ThePrimeagen/harpoon",
+  event = "VeryLazy",
+  branch = "harpoon2",
+  init = function ()
+    local harpoon = require("harpoon")
+    harpoon:setup()
+
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(require("Harpoon"):list()) end)
+    vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
+  end
+} ]]
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,6 +4,7 @@ return {
 	init = function()
 		require("which-key").register({
 			f = { name = "Telescope" },
+      h = { name = "Harpoon" },
 			l = { "<cmd>Lazy<cr>", "Lazy" },
 		}, { prefix = "<leader>" })
 	end,
