b57ea6cd5373a858f34cdaae27b2f1b3232adc9b
veryfastman <dony357@outlook.com>
2023-12-25 11:48:39 -0800

    removed auto install clangd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,7 @@ return {
 		require("neodev").setup()
 
 		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "clangd" },
+			ensure_installed = { "lua_ls" },
 		})
 
 		require("mason-lspconfig").setup_handlers({
