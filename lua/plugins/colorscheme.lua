dc5dbe311eea83b9a2c243610922ddb50d9561f6
veryfastman <dony357@outlook.com>
2023-12-29 15:45:06 -0800

    changed default colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,12 @@
 return {
 	"ellisonleao/gruvbox.nvim",
+  dependencies = {
+    "kaicataldo/material.vim"
+  },
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("gruvbox")
+		-- vim.cmd.colorscheme("gruvbox")
+    vim.cmd.colorscheme("material")
 	end,
 }
