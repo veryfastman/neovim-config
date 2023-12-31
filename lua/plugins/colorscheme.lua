ee2df8c8e7fd482ca43a71cf80db9c1e94e6caa5
veryfastman <dony357@outlook.com>
2023-12-30 23:06:36 -0800

    removed material colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
 	"ellisonleao/gruvbox.nvim",
-  dependencies = {
-    "kaicataldo/material.vim"
-  },
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- vim.cmd.colorscheme("gruvbox")
-    vim.cmd.colorscheme("material")
+		vim.cmd.colorscheme("gruvbox")
 	end,
 }
