4a10c0408aa51a099eef50161bcda8ed10f85233
veryfastman <dony357@outlook.com>
2024-01-07 20:51:01 -0800

    added rust mappings & moved refactoring to unused plugins

--- ftplugin/rs.lua
+++ ftplugin/rs.lua
@@ -1,4 +1,5 @@
 local options = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<leader>cc", ":!tmux next-window \\; send-keys 'cargo build'<CR>", options)
-vim.keymap.set("n", "<leader>cv", ":!tmux next-window \\; send-keys 'cargo run'<CR>", options)
+vim.keymap.set("n", "<leader>cc", ":!tmux next-window \\; send-keys 'cargo build' Enter<CR>", options)
+vim.keymap.set("n", "<leader>cv", ":!tmux next-window \\; send-keys 'cargo run' Enter<CR>", options)
+vim.keymap.set("n", "<leader>cb", ":!tmux next-window \\; send-keys \"cargo run --bin $(echo % | sed -e 's/.*\\///' -e 's/\\..*//')\" Enter<CR>", options)
