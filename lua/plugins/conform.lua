59bd612421c125f94e4081bf0300d5ab3afacc53
veryfastman <dony357@outlook.com>
2024-01-07 21:08:38 -0800

    added a formatter plugin

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,11 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			rust = { "rustfmt" },
+		},
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+	},
+}
