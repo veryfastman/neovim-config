4ad6480fc94897f80c8f61710c10ee72c010da6e
veryfastman <dony357@outlook.com>
2024-01-16 19:52:16 -0800

    commit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,13 @@
 return {
   "ellisonleao/gruvbox.nvim",
+  dependencies = {
+    "aktersnurra/no-clown-fiesta.nvim",
+    "Mofiqul/vscode.nvim",
+  },
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("gruvbox")
+    -- vim.cmd.colorscheme("gruvbox")
+    vim.cmd.colorscheme("vscode")
   end,
 }
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       rust = { "rustfmt" },
+      c = { "clang-format" },
     },
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
   },
