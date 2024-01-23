c8abc7b9fe5902d9f8b28f14610164f751398f47
veryfastman <dony357@outlook.com>
2024-01-22 21:55:30 -0800

    added rust keybinding

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,5 +1,6 @@
 local options = { noremap = true, silent = true }
 
+vim.keymap.set("n", "<leader>ca", ":!tmux next-window \\; send-keys 'cargo clippy' Enter<CR>", options)
 vim.keymap.set("n", "<leader>cc", ":!tmux next-window \\; send-keys 'cargo build' Enter<CR>", options)
 vim.keymap.set("n", "<leader>cv", ":!tmux next-window \\; send-keys 'cargo run' Enter<CR>", options)
 vim.keymap.set(
