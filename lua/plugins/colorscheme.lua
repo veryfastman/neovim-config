a3141687e9725ab4291dcd45cb0189847408484c
veryfastman <dony357@outlook.com>
2024-01-25 20:35:30 -0800

    using new solarized dark now

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,8 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  dependencies = {
-    "aktersnurra/no-clown-fiesta.nvim",
-    "Mofiqul/vscode.nvim",
-    "navarasu/onedark.nvim",
-  },
+  "Tsuzat/neosolarized.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require("onedark").setup({
-      style = "darker",
-    })
-    vim.cmd.colorscheme("vscode")
+    vim.cmd.colorscheme("NeoSolarized")
   end,
 }
