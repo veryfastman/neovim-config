31280b7633b9c72ad87a40bfa1f6e34967870c87
veryfastman <dony357@outlook.com>
2024-02-03 14:09:49 -0800

    gruvbox colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
-  "Tsuzat/NeoSolarized.nvim",
+  "ellisonleao/gruvbox.nvim",
   lazy = false,
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme("NeoSolarized")
+    require("gruvbox").setup {
+      transparent_mode = true
+    }
+    vim.cmd.colorscheme("gruvbox")
   end
 }
