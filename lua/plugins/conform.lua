11a62c008c511c9259e10390213e0eac2f3f6c88
veryfastman <dony357@outlook.com>
2024-02-27 20:28:00 -0800

    added formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
       lua = { "stylua" },
       rust = { "rustfmt" },
       c = { "clang-format" },
+      nix = { "nixpkgs-fmt" },
     },
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
   },
